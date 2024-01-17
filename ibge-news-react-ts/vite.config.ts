699c876df4dea04b1a58dad831ef80eaf7c87576
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-01-17 14:11:09 -0300

    inserindo / no base do vite.config

--- ibge-news-react-ts/vite.config.ts
+++ ibge-news-react-ts/vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/desafio-trybe-frontend-ibge-news-react-ts',
+  base : '/desafio-trybe-frontend-ibge-news-react-ts/',
   test: {
     globals: true,
     environment: 'jsdom',
